2d087789c7fa6e42d9b12dcbb8bc10e999150696
kamillo56 <111827018+kamillo56@users.noreply.github.com>
2022-08-23 09:26:25 +0200

    Update allegro.py

--- allegro.py
+++ allegro.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import sqlite3
 
+#Baza danych
 db = sqlite3.connect('Domy.db')
 cursor = db.cursor()
 
@@ -18,14 +19,17 @@ except:
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+#Tworze zmienną żeby nie przepisywać wiele razy
 wait = WebDriverWait(driver, 10)
 
 URL = driver.get("https://allegro.pl/kategoria/domy-na-sprzedaz-112740?powierzchnia-dzialki-do=10000&powierzchnia-dzialki-od=1")
 print(driver.title)
 
+#Zgoda cookies
 zgoda = driver.find_element(By.XPATH, '//*[@id="opbox-gdpr-consents-modal"]/div/div[2]/div/div[2]/button[2]')
 zgoda.click()
 
+#Region kujawsko-pomorkskie
 kujPom = driver.find_element(By.XPATH, '//*[@id="filters"]/div[1]/div/div/div/section/div[2]/ul/li[2]/div/a')
 kujPom.click()
 
@@ -45,7 +49,8 @@ for i in range(1, pages):
         powierzchniaC = wait.until(
         EC.presence_of_element_located((By.XPATH, f'//*[@id="search-results"]/div[6]/div/div/div/div/div/section[2]/article[{oferta}]/div/div/div[2]/div[1]/div[3]/div/dl/dd[2]'))
         ).text
-
+        
+        #Nie we wszystkich ogłoszeniach była ta wartość co zwracało błąd
         try:
             powierzchniaU = driver.find_element(By.XPATH, f'//*[@id="search-results"]/div[6]/div/div/div/div/div/section[2]/article[{oferta}]/div/div/div[2]/div[1]/div[3]/div/dl/dd[1]').text
         except: NoSuchElementException
@@ -54,9 +59,12 @@ for i in range(1, pages):
         EC.presence_of_element_located((By.XPATH, f'//*[@id="search-results"]/div[6]/div/div/div/div/div/section[2]/article[{oferta}]/div/div/div[2]/div[3]/div[1]'))
         ).text
         lokalizacja = lokalizacja.split(': ')[1]
-
+        
+        #Zapis do bazy danych
         cursor.execute('INSERT INTO Domy VALUES (?, ?, ?, ?, ?)', (tytul, lokalizacja, powierzchniaC, powierzchniaU, cena))
     db.commit()
+    
+    #Przełączanie stron, gdy jesteśmy na stronie 1 przycisk ma xpath a, gdy jesteśmy na 2 lub dalej przycisk już ma xpath a[2], stąd ten if.
     if i == 1:
         next = driver.find_element(By.XPATH, '//*[@id="search-results"]/div[7]/div[1]/div/div/div[2]/a')
     else:
